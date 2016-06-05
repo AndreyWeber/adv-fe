8356138d0f531617f3c8dd890f6901db5d3d6f91
Andrey Weber <Andrey.Weber@gmail.com>
2016-06-05 15:27:24 +0200

    Implemented home task8 -> obligatory task1

--- task8/app.js
+++ task8/app.js
@@ -1,17 +1,17 @@
-var express = require( 'express' );
-var path = require( 'path' );
-var fs = require( 'fs' );
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
 var url = require('url');
+var find = require('findit');
 var apiVersion = require('./package').version;
 
 var app = express();
 
-app.set('port', 5000);
+app.set('port', process.env.npm_package_config_port || 5000);
 
 app.listen(app.get('port'), function() {
-    console.log('Node app is running on http://localhost:' + app.get('port') );
+    console.log('Node app is running on http://localhost:' + app.get('port'));
 });
-
 
 app.get('/', function (req, res) {
     var urlParsed = url.parse(req.url, true);
@@ -25,45 +25,93 @@ app.all('/test/', function (req, res) {
     res.send('<html><body><h1>Hello test</h1></body></html>');
 });
 
-
-
-app.all('/api/' + apiVersion + '/*', function (req, res) {
-    // console.log(req);
-
-    render(req, res);
+app.all('/api/' + apiVersion + '/users', function (req, res) {
+    renderSeveral(req, res);
 });
 
+app.all('/api/' + apiVersion + '/users/*', function (req, res) {
+    renderSingle(req, res);
+});
 
+app.all('/api/' + apiVersion + '/posts', function (req, res) {
+    renderSeveral(req, res);
+});
 
+app.all('/api/' + apiVersion + '/posts/*', function (req, res) {
+    renderSingle(req, res);
+});
 
+function renderSeveral (req, res) {
+    var dirName = req.path.replace('/' + apiVersion + '/', '/');
+    //   /api/users/
+    var dirPath = path.join(__dirname, dirName);
+    //   /Users/puzankov/work/fs/node-js-getting-started/api/users/
 
+    var finder = find(dirPath);
+    var output = [];
+    finder.on('directory', function (dir, stat, stop) {
+        if (dirPath === dir) {
+            return;
+        }
+        var filePath = path.join(dir, req.method.toLowerCase() + '.json');
+        try {
+            fs.statSync(filePath);
 
+            var json = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+            if (Array.isArray(json)){
+                json.forEach(e => output.push(e));
+            } else {
+                output.push(json);
+            }
+        } catch (e) {
+            console.log('File not found', filePath);
+        }
+    });
 
+    finder.on('end', function () {
+        console.log('Succeeded');
 
+        res.setHeader('content-type', 'application/json');
+        res
+            .status(200)
+            .json(output)
+            .end();1
+    });
 
+    finder.on('error', function (err) {
+        console.log('Error occured', err);
 
+        res
+            .status(500)
+            .json([
+                {
+                    "info": {
+                        "success": false,
+                        "code": 12345
+                    }
+                }
+            ])
+            .end();
 
+    });
+}
 
-
-
-
-function render(req, res) {
-
+function renderSingle(req, res) {
     var fileName = req.path + '/' + req.method.toLowerCase() + '.json';
     //   /api/1.0.1/users/get.json
     fileName = fileName.replace('/' + apiVersion + '/', '/');
     //   /api/users/get.json
     var filePath = path.join(__dirname, fileName);
-    console.log(req.method, filePath);
     // /Users/puzankov/work/fs/node-js-getting-started/api/users/get.json
 
-    if (fs.statSync(filePath)) {
+    console.log(req.method, filePath);
+
+    try {
+        fs.statSync(filePath);
 
         res.setHeader('content-type', 'application/json');
-
         fs.createReadStream(filePath).pipe(res);
-    }
-    else {
+    } catch (e) {
         console.log('no such file', filePath);
 
         res
